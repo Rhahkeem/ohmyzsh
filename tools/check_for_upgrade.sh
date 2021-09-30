d176e2f673f170126a3bfcdd37059f5434678e93
Marc Cornellà <hello@mcornella.com>
2021-09-30 17:56:02 +0200

    feat(updater): check if there are updates before prompting (#8387)
    Fixes #8387

--- tools/check_for_upgrade.sh
+++ tools/check_for_upgrade.sh
@@ -34,11 +34,11 @@ function current_epoch() {
 
 function is_update_available() {
   local branch
-  branch=${"$(builtin cd -q "$ZSH"; git config --local oh-my-zsh.branch)":-master}
+  branch=${"$(git -C "$ZSH" config --local oh-my-zsh.branch)":-master}
 
   local remote remote_url remote_repo
-  remote=${"$(builtin cd -q "$ZSH"; git config --local oh-my-zsh.remote)":-origin}
-  remote_url=$(builtin cd -q "$ZSH"; git config remote.$remote.url)
+  remote=${"$(git -C "$ZSH" config --local oh-my-zsh.remote)":-origin}
+  remote_url=$(git -C "$ZSH" config remote.$remote.url)
 
   local repo
   case "$remote_url" in
@@ -54,36 +54,17 @@ function is_update_available() {
   [[ "$repo" = ohmyzsh/ohmyzsh ]] || return 0
   local api_url="https://api.github.com/repos/${repo}/commits/${branch}"
 
-  # Get local HEAD. If this fails assume there are updates
-  local local_head
-  local_head=$(builtin cd -q "$ZSH"; git rev-parse $branch 2>/dev/null) || return 0
+  # Get local and remote HEADs and compare them. If we can't get either assume there are updates
+  local local_head remote_head
+  local_head=$(git -C "$ZSH" rev-parse $branch 2>/dev/null) || return 0
 
-  # Get remote HEAD. If no suitable command is found assume there are updates
-  # On any other error, skip the update (connection may be down)
-  local remote_head
-  remote_head=$(
-    if (( ${+commands[curl]} )); then
-      curl --connect-timeout 2 -fsSL -H 'Accept: application/vnd.github.v3.sha' $api_url 2>/dev/null
-    elif (( ${+commands[wget]} )); then
-      wget -T 2 -O- --header='Accept: application/vnd.github.v3.sha' $api_url 2>/dev/null
-    elif (( ${+commands[fetch]} )); then
-      HTTP_ACCEPT='Accept: application/vnd.github.v3.sha' fetch -T 2 -o - $api_url 2>/dev/null
-    else
-      exit 0
-    fi
-  ) || return 1
+  remote_head=$(curl -fsSL -H 'Accept: application/vnd.github.v3.sha' $api_url 2>/dev/null) \
+  || remote_head=$(wget -O- --header='Accept: application/vnd.github.v3.sha' $api_url 2>/dev/null) \
+  || remote_head=$(HTTP_ACCEPT='Accept: application/vnd.github.v3.sha' fetch -o - $api_url 2>/dev/null) \
+  || return 0
 
-  # Compare local and remote HEADs (if they're equal there are no updates)
-  [[ "$local_head" != "$remote_head" ]] || return 1
-
-  # If local and remote HEADs don't match, check if there's a common ancestor
-  # If the merge-base call fails, $remote_head might not be downloaded so assume there are updates
-  local base
-  base=$(builtin cd -q "$ZSH"; git merge-base $local_head $remote_head 2>/dev/null) || return 0
-
-  # If the common ancestor ($base) is not $remote_head,
-  # the local HEAD is older than the remote HEAD
-  [[ $base != $remote_head ]]
+  # Compare local and remote HEADs
+  [[ "$local_head" != "$remote_head" ]]
 }
 
 function update_last_updated_file() {
@@ -148,6 +129,8 @@ function has_typed_input() {
   #  the shell actually exits what it's running.
   trap "
     ret=\$?
+    unset update_mode
+    unset -f current_epoch is_update_available update_last_updated_file update_ohmyzsh 2>/dev/null
     command rm -rf '$ZSH/log/update.lock'
     return \$ret
   " EXIT INT QUIT
@@ -162,6 +145,11 @@ function has_typed_input() {
   zstyle -s ':omz:update' frequency epoch_target || epoch_target=${UPDATE_ZSH_DAYS:-13}
   # Test if enough time has passed until the next update
   if (( ( $(current_epoch) - $LAST_EPOCH ) < $epoch_target )); then
+    return
+  fi
+
+  # Check if there are updates available before proceeding
+  if ! is_update_available; then
     return
   fi
 
@@ -214,4 +202,4 @@ function has_typed_input() {
 }
 
 unset update_mode
-unset -f current_epoch update_last_updated_file update_ohmyzsh
+unset -f current_epoch is_update_available update_last_updated_file update_ohmyzsh
