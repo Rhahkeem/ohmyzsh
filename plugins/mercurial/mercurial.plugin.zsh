96bcb83943e0ff8efb44321f359e6b059292c999
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-10-04 09:55:18 -0400

    feat(mercurial): add `hg log` aliases (#9979)
    Co-authored-by: Michał Kielan <kielanm@fb.com>

--- plugins/mercurial/mercurial.plugin.zsh
+++ plugins/mercurial/mercurial.plugin.zsh
@@ -21,6 +21,45 @@ alias hgo='hg outgoing'
 alias hglg='hg log --stat -v'
 alias hglgp='hg log --stat  -p -v'
 
+function in_hg() {
+  if $(hg branch > /dev/null 2>&1); then
+    echo 1
+  fi
+}
+
+function hg_get_branch_name() {
+  branch=`hg branch 2>/dev/null`
+  if [ $? -eq 0 ]; then
+    echo $branch
+  fi
+  unset branch
+}
+
+function hg_prompt_info {
+  _DISPLAY=`hg branch 2>/dev/null`
+  if [ $? -eq 0 ]; then
+    echo "$ZSH_PROMPT_BASE_COLOR$ZSH_THEME_HG_PROMPT_PREFIX\
+$ZSH_THEME_REPO_NAME_COLOR$_DISPLAY$ZSH_PROMPT_BASE_COLOR$ZSH_PROMPT_BASE_COLOR$(hg_dirty)$ZSH_THEME_HG_PROMPT_SUFFIX$ZSH_PROMPT_BASE_COLOR"
+  fi
+  unset _DISPLAY
+}
+
+function hg_dirty_choose {
+  hg status -mar 2> /dev/null | command grep -Eq '^\s*[ACDIM!?L]'
+  if [ $? -eq 0 ]; then
+    if [ $pipestatus[-1] -eq 0 ]; then
+      # Grep exits with 0 when "One or more lines were selected", return "dirty".
+      echo $1
+      return
+    fi
+  fi
+  echo $2
+}
+
+function hg_dirty {
+  hg_dirty_choose $ZSH_THEME_HG_PROMPT_DIRTY $ZSH_THEME_HG_PROMPT_CLEAN
+}
+
 function hgic() {
   hg incoming "$@" | grep "changeset" | wc -l
 }
